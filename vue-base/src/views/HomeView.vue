d741f8ffc419f550cecaf3cdbff6a3de98872b29
pengzhijian <13087545742>
2024-04-19 17:59:21 +0800

    项目初始化

--- vue-base/src/views/HomeView.vue
+++ vue-base/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+console.log('window.jinlin', window.jinlin.pika)
+const pika = window.jinlin.pika;
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    home
+    <div>我的精灵是：{{ pika }}</div>
+    <!-- <div>我的精灵是：{{ window.jinlin.pika }}</div> --> 
+  </div>
 </template>
